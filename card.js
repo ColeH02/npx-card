5c7ffe6ca2413ad2bcef51c72e6f08d0fd3548e2
Cole Hausman <colehausman@gmail.com>
2023-04-13 14:14:06 -0400

    card finished

--- card.js
+++ card.js
@@ -38,18 +38,6 @@ const questions = [
                         text: ' Downloading Resume',
                         spinner: cliSpinners.material,
                     }).start();
-                    /*
-                    const download = new DownloaderHelper('http://github.com/ColeH02/about-me/blob/main/ColeHausmanResume.pdf', './');
-
-                    download.on("end", function () {
-                        let downloadPath = path.join(process.cwd(), 'cole_hausman-resume.pdf')
-                        console.log(`\nResume Downloaded at ${downloadPath} \n`);
-                        open(downloadPath)
-                        loader.stop();
-                    });
-                    download.start();
-                    */
-
                     let pipe = request('https://raw.githubusercontent.com/ColeH02/about-me/main/ColeHausmanResume.pdf').pipe(fs.createWriteStream('./cole_hausman-resume.pdf'));
                     pipe.on("finish", function () {
                         let downloadPath = path.join(process.cwd(), 'cole_hausman-resume.pdf')
@@ -71,16 +59,16 @@ const questions = [
 
 const data = {
     name: chalk.hex('#ffffff').bold("                     Cole Hausman"),
-    fact: chalk.hex('#d2dee4')('If at first you dont succeed try try try try try again'),
-    email: chalk.hex('#a6bdc9')("colehausman@gmail.com"),
-    github: chalk.hex('#7b9eaf')("https://github.com/ColeH02"),
-    website: chalk.hex('#4d7f96')("https://colehausman.com"),
-    npx: chalk.hex('#0e627d')("npx colehausman"),
+    fact: chalk.hex('#fafa6e')('If at first you dont succeed try try try try try again'),
+    email: chalk.hex('#c4ec74')("colehausman@gmail.com"),
+    github: chalk.hex('#92dc7e')("https://github.com/ColeH02"),
+    website: chalk.hex('#64c987')("https://www.linkedin.com/in/cole-hausman"),
+    npx: chalk.hex('#39b48e')("npx cole_hausman"),
 
     labelFact: chalk.hex('#e407d2').bold("          Fun Fact:"),
     labelEmail: chalk.hex('#be0fc4').bold("          Email:"),
     labelGitHub: chalk.hex('#9912b5').bold("         GitHub:"),
-    labelWebsite: chalk.hex('#7513a4').bold("        Website:"),
+    labelWebsite: chalk.hex('#7513a4').bold("       LinkedIn:"),
     labelCard: chalk.hex('#511191').bold("                  Card:")
 };
 
@@ -97,16 +85,14 @@ const me = boxen(
         `${data.labelCard}  ${data.npx}`,
         ``,
         `${chalk.bold(
-            "Hi there! I'm Cole, I'm a passionate developer!"
+            "Hi there! I'm Cole, a passionate developer "
         )}`,
-        `${chalk.bold("Student at Bucknell University and currently")}`,
-        `${chalk.bold(
-            "working as a ServiceNow Intern. Hit me up if you"
-        )}`,
-        `${chalk.bold(
-            "want to connect!"
-        )}`
-    ].join("\n"),
+        `${chalk.bold("who is always looking to connect! I'm a")}`,
+        `${chalk.bold("Student at Bucknell University and currently")}`
+    ].join("\n") + "\n" +
+    chalk.bold("working as a ") +
+    chalk.hex('#e407d2').bold("ServiceNow") +
+    chalk.bold(" Software Engineering Intern."),
     {
         margin: 1,
         float: 'center',
